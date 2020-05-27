9e4bc4183e3a20b8d7f4f75525a46ef69b3dad20
Johannes Bader <olydis@users.noreply.github.com>
2020-05-27 13:07:26 -0700

    race condition?

--- test/azure/src/main/java/fixtures/head/implementation/AutoRestHeadTestServiceImpl.java
+++ test/azure/src/main/java/fixtures/head/implementation/AutoRestHeadTestServiceImpl.java
@@ -152,4 +152,50 @@ public class AutoRestHeadTestServiceImpl extends AzureServiceClient implements A
     public String userAgent() {
         return String.format("%s (%s, %s)", super.userAgent(), "AutoRestHeadTestService", "1.0.0");
     }
+    
+class Counter  implements Runnable{
+    private int c = 0;
+
+    public void increment() {
+        try {
+            Thread.sleep(10);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        c++;
+    }
+
+    public void decrement() {    
+        c--;
+    }
+
+    public int getValue() {
+        return c;
+    }
+    
+    @Override
+    public void run() {
+        //incrementing
+        this.increment();
+        System.out.println("Value for Thread After increment " 
+        + Thread.currentThread().getName() + " " + this.getValue());
+        //decrementing
+        this.decrement();
+        System.out.println("Value for Thread at last " 
+        + Thread.currentThread().getName() + " " + this.getValue());        
+    }
+}
+
+public class RaceConditionDemo{
+    public static void test() {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(counter, "Thread-1");
+        Thread t2 = new Thread(counter, "Thread-2");
+        Thread t3 = new Thread(counter, "Thread-3");
+        t1.start();
+        t2.start();
+        t3.start();
+    }    
+}
 }
